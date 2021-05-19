fa61148821636968f2711fc7704f84f1f4ac7e8c
Wanding Zhou - Bioinformatics <zhouwanding@gmail.com>
2021-05-19 17:48:39 -0400

    Update 1. Installation.R

--- 1. Installation.R
+++ 1. Installation.R
@@ -4,15 +4,16 @@
 ## SeSAMe
 ## https://bioconductor.org/packages/release/bioc/html/sesame.html
 
+## MethylSuite (for Python Users)
+## https://pypi.org/project/methylsuite/
+
+## Infinium_Methylation_Workhorse
+## https://github.com/bbarnes/Infinium_Methylation_Workhorse
+
 install.packages("BiocManager")
 BiocManager::install("sesame")
 library(sesame)
 sesameDataCache("EPIC")
 sesameDataCacheAll()
 
-## Python Users
-## MethylSuite
-## https://pypi.org/project/methylsuite/
 
-## Infinium_Methylation_Workhorse
-## https://github.com/bbarnes/Infinium_Methylation_Workhorse
